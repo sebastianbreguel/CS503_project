907e4e3c4bc920c3e6260fb7d0d2743c17460efb
sebastianbreguel <breguelsebastian@gmail.com>
2023-05-11 20:16:57 +0200

    Delete transformer file

--- Layers/__init__.py
+++ Layers/__init__.py
@@ -1,6 +1,6 @@
-from .layerss import (Attention, ConvAttention, Custom_transformer,
-                      CustomBlock, Mlp, MultiDPHConvHeadAttention,
-                      Parallel_blocks, Parallel_transformers, RobustAttention,
-                      Transformer)
 from .patch_embeddings import ConvEmbedding, NaivePatchEmbed
 from .positional_encodings import SineCosinePosEmbedding
+from .transformers import (Attention, ConvAttention, Custom_transformer,
+                           CustomBlock, Mlp, MultiDPHConvHeadAttention,
+                           Parallel_blocks, Parallel_transformers,
+                           RobustAttention, Transformer)
