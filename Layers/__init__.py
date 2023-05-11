873364d10e8a738224741b36b4a7bbcf63012824
sebastianbreguel <breguelsebastian@gmail.com>
2023-05-11 10:29:56 +0200

    formarter  ✨ 🍰 ✨

--- Layers/__init__.py
+++ Layers/__init__.py
@@ -1,11 +1,14 @@
 from .patch_embeddings import ConvEmbedding, NaivePatchEmbed
 from .positional_encodings import SineCosinePosEmbedding
 from .transformers import (
+    # Self-attention layers
     Attention,
     ConvAttention,
     Mlp,
+    # Transformers blocks
     ConvBlock,
     Parallel_block,
+    # Transformers
     Parallel_transformers,
     Custom_transformer,
     Transformer,
